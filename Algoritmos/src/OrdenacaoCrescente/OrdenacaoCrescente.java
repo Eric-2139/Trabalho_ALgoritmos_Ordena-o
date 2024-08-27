1cdc7af0def91744150b7a663167c969b17cf710
Sarah Mota <sarahcoelhomota@gmail.com>
2024-08-26 21:50:22 -0300

    add new changes

--- Algoritmos/src/OrdenacaoCrescente/OrdenacaoCrescente.java
+++ Algoritmos/src/OrdenacaoCrescente/OrdenacaoCrescente.java
@@ -14,9 +14,11 @@ public class OrdenacaoCrescente {
                 }
             }
         }
+
     }
 
     public void insertionSort(int[] arr) {
+        
 
         for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
@@ -27,10 +29,13 @@ public class OrdenacaoCrescente {
             }
             arr[j + 1] = key;
         }
+
+        
     }
 
     public void selectionSort(int[] arr) {
        
+
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
@@ -44,16 +49,19 @@ public class OrdenacaoCrescente {
             arr[minIdx] = arr[i];
             arr[i] = temp;
         }
+
+        
     }
 
     public void mergeSort(int[] arr, int l, int r) {
-
+        
         if (l < r) {
             int m = (l + r) / 2;
             mergeSort(arr, l, m);
             mergeSort(arr, m + 1, r);
             merge(arr, l, m, r);
         }
+
     }
 
     private static void merge(int[] arr, int l, int m, int r) {
@@ -95,7 +103,44 @@ public class OrdenacaoCrescente {
         }
     }
 
+    /*
+    public void quickSort(int[] vetor) {
+        quickSort(vetor, 0, vetor.length);
+    }
+
+    public void quickSort(int[] vetor, int ini, int fim) {
+        int i = ini;
+        int j = fim - 1;
+        if (i >= j) {
+            return;
+        }
+        int pivo = vetor[(ini + fim) / 2];
+
+        while (i < j) {
+            while (vetor[i] < pivo) {
+                i++;
+            }
+            while (vetor[j] > pivo) {
+                j--;
+            }
+            if (i < j) {
+                int aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+                i++;
+                j--;
+            }
+            if (ini < j) {
+                quickSort(vetor, ini, j);
+            }
+            if (i < fim) {
+                quickSort(vetor, i, fim);
+            }
+        }
+    }
+*/
     public void quickSort(int[] arr, int low, int high) {
+        
 
         if (low < high) {
             int pi = partition(arr, low, high);
@@ -103,6 +148,8 @@ public class OrdenacaoCrescente {
             quickSort(arr, low, pi);
             quickSort(arr, pi + 1, high);
         }
+
+        
     }
 
     private static int partition(int[] arr, int low, int high) {
@@ -119,7 +166,8 @@ public class OrdenacaoCrescente {
                 j--;
             } while (arr[j] > pivot);
 
-            if (i >= j) return j;
+            if (i >= j)
+                return j;
 
             int temp = arr[i];
             arr[i] = arr[j];
@@ -128,6 +176,7 @@ public class OrdenacaoCrescente {
     }
 
     public void heapSort(int[] arr) {
+        double tempoInicial = System.currentTimeMillis();
 
         int n = arr.length;
 
@@ -141,6 +190,10 @@ public class OrdenacaoCrescente {
 
             heapify(arr, i, 0);
         }
+
+        double tempoFinal = System.currentTimeMillis();
+        double tempoExecucao = tempoFinal - tempoInicial;
+        System.out.println("Tempo de execução em milissegundos: " + tempoExecucao);
     }
 
     private void heapify(int[] arr, int n, int i) {
