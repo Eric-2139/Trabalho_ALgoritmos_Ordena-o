fd947f3ca0b51d1bacebee643a9c16b94222c0a6
Eric_2139 <ericpowercosta2005@gmail.com>
2024-09-14 16:05:35 -0300

    consertado erro generalizado de falta de interpretação de texto

--- Algoritmos/src/SortingExample.java
+++ Algoritmos/src/SortingExample.java
@@ -1,15 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-import OrdenacaoAleatoria.OrdenacaoAleatoria;
 import OrdenacaoCrescente.OrdenacaoCrescente;
-import OrdenacaoDecrescente.OrdenacaoDecrescente;
 
 public class SortingExample {
 
     static OrdenacaoCrescente ordenacaoCrescente = new OrdenacaoCrescente();
-    static OrdenacaoDecrescente ordenacaoDecrescente = new OrdenacaoDecrescente();
-    static OrdenacaoAleatoria ordenacaoAleatoria = new OrdenacaoAleatoria();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,26 +15,30 @@ public class SortingExample {
             System.out.println("");
 
             System.out.print(
-                    "Escolha o algoritmo: ordenação crescente->(1: BubbleSortC, 2: InsertionSortC, 3: SelectionSortC, 4: MergeSortC, 5: QuickSortC, 6: HeapSortC), ordenação descrecente->(7: BubbleSortD, 8: InsertionSortD, 9: SelectionSortD, 10: MergeSortD, 11: QuickSortD, 12: HeapSortD),-> ordenacao aleatorio(13: BubbleSortA, 14: InsertionSortA, 15: SelectionSortA, 16: MergeSortA, 17: QuickSortA, 18: HeapSortA, 19: encerrar aplicacao): ");
+                    "Escolha o algoritmo de ordenação crescente-> \n" +
+                            "1: BubbleSort \n" +
+                            "2: InsertionSort \n" +
+                            "3: SelectionSort \n" +
+                            "4: MergeSort \n" +
+                            "5: QuickSort \n" +
+                            "6: HeapSort \n" +
+                            "ou digite 7 para fechar o programa: ");
             int choice = scanner.nextInt();
 
-            if (choice == 19) {
+            if (choice == 7) {
                 break;
             }
 
             System.out.print("Digite a quantidade de números a serem ordenados: ");
             int n = scanner.nextInt();
 
-            /*
-             * System.out.print("Digite o nome do arquivo de entrada: ");
-             * String inputFileName = scanner.next();
-             * 
-             * System.out.print("Digite o nome do arquivo de saída: ");
-             * String outputFileName = scanner.next();
-             */
-            System.out.println("Selecione o arquivo de entrada:");
-            System.out.println("1 - numeros-com-repeticao.txt");
-            System.out.println("2 - numeros-sem-repeticao.txt");
+            System.out.println("Selecione o arquivo de entrada:\n" +
+                    "1: aleatorio_com_repeticao.txt \n" +
+                    "2: aleatorio_sem_repeticao.txt \n" +
+                    "3: crescente_com_repeticao.txt \n" +
+                    "4: crescente_sem_repeticao.txt \n" +
+                    "5: decrescente_com_repeticao.txt \n" +
+                    "6: decrescente_sem_repeticao.txt");
 
             int filechoice = scanner.nextInt();
             String inputFileName = "";
@@ -46,10 +46,22 @@ public class SortingExample {
             // Escolhe o arquivo de entrada com base na escolha do usuário
             switch (filechoice) {
                 case 1:
-                    inputFileName = "numeros-com-repeticao.txt";
+                    inputFileName = "aleatorio_com_repeticao.txt";
                     break;
                 case 2:
-                    inputFileName = "numeros-sem-repeticao.txt";
+                    inputFileName = "aleatorio_sem_repeticao.txt";
+                    break;
+                case 3:
+                    inputFileName = "crescente_com_repeticao.txt";
+                    break;
+                case 4:
+                    inputFileName = "crescente_sem_repeticao.txt";
+                    break;
+                case 5:
+                    inputFileName = "decrescente_com_repeticao.txt";
+                    break;
+                case 6:
+                    inputFileName = "decrescente_sem_repeticao.txt";
                     break;
                 default:
                     System.out.println("Opção inválida. O programa será encerrado.");
@@ -78,87 +90,34 @@ public class SortingExample {
                 // Ordenação Crescente
                 case 1:
                     ordenacaoCrescente.bubbleSort(numbers);
-                    algorithm = "BubbleSort Crescente";
+                    algorithm = "BubbleSort";
                     break;
                 case 2:
                     ordenacaoCrescente.insertionSort(numbers);
-                    algorithm = "Insertion Sort";
+                    algorithm = "Insertion";
                     break;
                 case 3:
                     ordenacaoCrescente.selectionSort(numbers);
-                    algorithm = "SelectionSort Crescente";
+                    algorithm = "SelectionSort";
                     break;
                 case 4:
                     ordenacaoCrescente.mergeSort(numbers, 0, n - 1);
-                    algorithm = "MergeSort Crescente";
+                    algorithm = "MergeSort";
                     break;
                 case 5:
                     ordenacaoCrescente.quickSort(numbers, 0, n - 1);
-                    algorithm = "QuickSort Crescente";
+                    algorithm = "QuickSort";
                     break;
                 case 6:
                     ordenacaoCrescente.heapSort(numbers);
-                    algorithm = "HeapSort Crescente";
+                    algorithm = "HeapSort";
                     break;
-
-                // Ordenação Decrescente
-                case 7:
-                    ordenacaoDecrescente.bubbleSort(numbers);
-                    algorithm = "BubbleSort Decrescente";
-                    break;
-                case 8:
-                    ordenacaoDecrescente.insertionSort(numbers);
-                    algorithm = "InsertionSort Decrescente";
-                    break;
-                case 9:
-                    ordenacaoDecrescente.selectionSort(numbers);
-                    algorithm = "SelectionSort Decrescente";
-                    break;
-                case 10:
-                    ordenacaoDecrescente.mergeSort(numbers, 0, n - 1);
-                    algorithm = "MergeSort Decrescente";
-                    break;
-                case 11:
-                    ordenacaoDecrescente.quickSort(numbers, 0, n - 1);
-                    algorithm = "QuickSort Decrescente";
-                    break;
-                case 12:
-                    ordenacaoDecrescente.heapSort(numbers);
-                    algorithm = "HeapSort Decrescente";
-                    break;
-
-                // Ordenação Aleatória (Embaralhamento)
-                case 13:
-                    ordenacaoAleatoria.bubbleShuffle(numbers);
-                    algorithm = "BubbleShuffle Aleatório";
-                    break;
-                case 14:
-                    ordenacaoAleatoria.insertionShuffle(numbers);
-                    algorithm = "InsertionShuffle Aleatório";
-                    break;
-                case 15:
-                    ordenacaoAleatoria.selectionShuffle(numbers);
-                    algorithm = "SelectionShuffle Aleatório";
-                    break;
-                case 16:
-                    ordenacaoAleatoria.mergeShuffle(numbers, 0, n - 1);
-                    algorithm = "MergeShuffle Aleatório";
-                    break;
-                case 17:
-                    ordenacaoAleatoria.quickShuffle(numbers, 0, n - 1);
-                    algorithm = "QuickShuffle Aleatório";
-                    break;
-                case 18:
-                    ordenacaoAleatoria.heapShuffle(numbers);
-                    algorithm = "HeapShuffle Aleatório";
-                    break;
-
                 default:
                     System.out.println("Algoritmo de ordenação inválido.");
             }
 
             double tempoFinal = System.currentTimeMillis();
-            double tempoExecucao = (tempoFinal - tempoInicial)/1000;
+            double tempoExecucao = (tempoFinal - tempoInicial) / 1000;
             System.out.println("Tempo de execução em segundos: " + tempoExecucao);
 
             // Gravar os números ordenados no arquivo de saída
@@ -173,61 +132,26 @@ public class SortingExample {
 
             System.out.println("Ordenação concluída e salva no arquivo: " + outputFileName);
 
-            switch (choice) {
+            switch (filechoice) {
                 case 1:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosBublleSortCrescente.txt");
+                    exportarResultados(inputFileName, algorithm, n, tempoExecucao, "resultados" + algorithm + "AleatorioComRepeticao.txt");
                     break;
                 case 2:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosInsertionSortCrescente.txt");
+                    exportarResultados(inputFileName, algorithm, n, tempoExecucao, "resultados" + algorithm + "AleatorioSemRepeticao.txt");
                     break;
                 case 3:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosSelectionCrescente.txt");
+                    exportarResultados(inputFileName, algorithm, n, tempoExecucao, "resultados" + algorithm + "CrescenteComRepeticao.txt");
                     break;
                 case 4:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosMergeSortCrescente.txt");
+                    exportarResultados(inputFileName, algorithm, n, tempoExecucao, "resultados" + algorithm + "CrescenteSemRepeticao.txt");
                     break;
                 case 5:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosQuickSortCrescente.txt");
+                    exportarResultados(inputFileName, algorithm, n, tempoExecucao, "resultados" + algorithm + "DecrescenteComRepeticao.txt");
                     break;
                 case 6:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosHeapSortCrescente.txt");
+                    exportarResultados(inputFileName, algorithm, n, tempoExecucao, "resultados" + algorithm + "DecrescenteSemRepeticao.txt");
                     break;
-                case 7:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosBublleSortDecrescente.txt");
-                    break;
-                case 8:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosInsertionSortDecrescente.txt");
-                    break;
-                case 9:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosSelectionSortDecrescente.txt");
-                    break;
-                case 10:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosMergeSortDecrescente.txt");
-                    break;
-                case 11:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosQuickSortDecrescente.txt");
-                    break;
-                case 12:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosHeapSortDescrescente.txt");
-                    break;
-                case 13:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosBublleSortAleatorio.txt");
-                    break;
-                case 14:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosInsertionSortAleatorio.txt");
-                    break;
-                case 15:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosSelectionAleatorio.txt");
-                    break;
-                case 16:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosMergeSortAleatorio.txt");
-                    break;
-                case 17:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosQuickSortAleatorio.txt");
-                    break;
-                case 18:
-                    exportarResultados(algorithm, n, tempoExecucao, "resultadosHeapAleatorio.txt");
-                    default:
+                default:
 
                     break;
             }
@@ -236,9 +160,9 @@ public class SortingExample {
         scanner.close();
     }
 
-    public static void exportarResultados(String algorithm, int numElements, double tempoExecucao, String outputFile) {
+    public static void exportarResultados(String inputFileName, String algorithm, int numElements, double tempoExecucao, String outputFile) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile, true))) {
-            bw.write(String.format("%-25s %-20d %.4f", algorithm, numElements, tempoExecucao));
+            bw.write(String.format("%-45s %-25s %-20d %.4f", inputFileName, algorithm, numElements, tempoExecucao));
             bw.newLine();
             System.out.println("Resultados exportados para: " + outputFile);
         } catch (IOException e) {
